a1f5756c8f111fd210dfa3c82359effe06b05ce5
Mr BDR <mirebordier@gmail.com>
2022-06-21 16:06:55 +0200

    start pf init

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,18 @@
 import "./App.scss";
 // Méthode 1 des images : on importe chaque image
-import dino from "./assets/dinosaur.png";
+// import bgweed1 from "./assets/bgweed1.jpg";
+import Loader from "./components/Loader";
 
 function App() {
   return (
     <div className="app">
-      <p className="app__text">
-        Ce template est bien configuré pour vos applications front-end avec
-        Vite, Sass et Vercel
-      </p>
+      {/* <img src="../../public/assets/bgweed2.jpg" alt="" srcset="" /> */}
+      <p className="app__text">Welcome To my RastaFolio !</p>
       {/* Méthode 1 : on peut donc directement utiliser son nom */}
-      <img width={150} height={150} src={dino} alt="" />
+      {/* <img width={150} height={150} src={bgweed1} alt="" /> */}
       {/* Méthode 2 : après création du dossier "public" à la racine, on peut utiliser le chemin relatif */}
-      <img width={150} height={150} src="./assets/trex.png" alt="" />
+      {/* <img width={150} height={150} src="./assets/bgweed2.jpg" alt="" /> */}
+      <Loader />
     </div>
   );
 }
